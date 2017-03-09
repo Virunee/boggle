12016db58162c307dd959e6a046fa326e1464714
Virunee <viravongv@gmail.com>
2017-03-09 16:05:49 +0000

    Add initial tests
    Added initial tests and function that gets neighours of a position

--- boggle.py
+++ boggle.py
@@ -1,7 +1,26 @@
+from string import ascii_uppercase
+from random import choice
+
+
 def make_grid(width, height):
-    return {(row,col): ' '
+    return {(row, col): choice(ascii_uppercase)
             for row in range(height)
             for col in range(width)}
 
-grid = make_grid(2,2)
+
+def neighbours_of_a_position((row, col)):
+    return [(row - 1, col - 1), (row - 1, col), (row - 1, col + 1),
+            (row, col - 1),                     (row, col + 1),
+            (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)]
+
+def all_grid_neighbours(grid):
+    neighbours = {}
+    for position in grid:
+        position_neighbours = neighbours_of_a_position(position)
+        neighbours[position] = [p for p in position_neighbours if p in grid]
+    return neighbours
+
+grid = make_grid(2, 2)
 print grid
+print neighbours_of_a_position((1, 1))
+print all_grid_neighbours(grid)
